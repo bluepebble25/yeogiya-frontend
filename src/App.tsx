5c4edc5a36459231961b70ee9cb752d308aa812b
bluepebble25 <bluepebble25@gmail.com>
2023-07-20 14:49:23 +0900

    ✨ 네비게이션바 컴포넌트 생성 및 레이아웃에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ListPage from './pages/ListPage';
+import Nav from './components/blocks/Nav';
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
   return (
     <Routes>
-      <Route index element={<MainPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/list" element={<ListPage />} />
+      </Route>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/list" element={<ListPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
